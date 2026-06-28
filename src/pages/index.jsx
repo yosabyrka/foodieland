43496a380425a33da75c4ec4b50cff838a19cf10
Sabyrzhan Zhaslanuly <yosabyrka@gmail.com>
2026-06-28 20:47:31 +0500

    feat: add SubscribeCard component (#8)
    * feat: add SubscribeCard markup
    
    * feat: add SubscribeCard styles
    
    * feat: adapt SubscribeCard for mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Hero from '@/sections/Hero'
+import SubscribeCard from '@/sections/SubscribeCard'
 
 export const metadata = {
   title: 'Home',
@@ -8,6 +9,7 @@ export default () => {
   return (
     <>
       <Hero />
+      <SubscribeCard />
     </>
   )
 }
