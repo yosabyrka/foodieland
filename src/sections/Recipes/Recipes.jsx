809f957d3f5482514154f8af3971dd71210250cf
Sabyrzhan Zhaslanuly <yosabyrka@gmail.com>
2026-07-01 00:24:01 +0500

    fix: delete unused recipes styles file

--- src/sections/Recipes/Recipes.jsx
+++ src/sections/Recipes/Recipes.jsx
@@ -3,12 +3,10 @@ import Grid from '@/components/Grid'
 import RecipeCard from '@/components/RecipeCard'
 import PromoBanner from '@/components/PromoBanner'
 import { recipes } from './recipes.data'
-import './Recipes.scss'
 
 export default () => {
   return (
     <Section
-      className="recipes"
       title="Simple and tasty recipes"
       titleId="recipes-title"
       description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
