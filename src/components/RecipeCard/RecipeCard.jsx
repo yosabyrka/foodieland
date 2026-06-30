d49386a95651afacdd451a68480d288a3fc4d780
Sabyrzhan Zhaslanuly <yosabyrka@gmail.com>
2026-06-30 20:34:59 +0500

    feat: add recipe card

---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,23 @@
+import { Image } from 'minista'
+import Tags from '@/components/Tags'
+import likedIcon from '@/assets/icons/liked.svg'
+import unlikedIcon from '@/assets/icons/unliked.svg'
+import './RecipeCard.scss'
+
+export default ({ title, imgSrc, isLiked, tags }) => {
+  return (
+    <article className="recipe-card">
+      <div className="recipe-card__image-wrapper">
+        <Image
+          className="recipe-card__liked-icon"
+          src={isLiked ? likedIcon : unlikedIcon}
+        />
+        <Image className="recipe-card__image" src={imgSrc} />
+      </div>
+      <div className="recipe-card__body">
+        <h3 className="recipe-card__title h5">{title}</h3>
+        <Tags tags={tags} mode="transparent" />
+      </div>
+    </article>
+  )
+}
