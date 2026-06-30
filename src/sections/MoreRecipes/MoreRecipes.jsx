a7faf76e74f4545b66976bf1eda066a06d1a8b27
Sabyrzhan Zhaslanuly <yosabyrka@gmail.com>
2026-07-01 00:23:27 +0500

    feat: add more recipes section

---
+++ src/sections/MoreRecipes/MoreRecipes.jsx
@@ -0,0 +1,22 @@
+import Section from '@/layouts/Section'
+import Grid from '@/components/Grid'
+import RecipeCard from '@/components/RecipeCard'
+import { moreRecipes } from './more-recipes.data'
+import './MoreRecipes.scss'
+
+export default () => {
+  return (
+    <Section
+      title="Try this delicious recipe to make your day"
+      titleId="more-recipes-title"
+      titleMode="justify-between"
+      description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim"
+    >
+      <Grid columns={4}>
+        {moreRecipes.map(({ id, ...recipe }) => (
+          <RecipeCard key={id} mode="alt" {...recipe} />
+        ))}
+      </Grid>
+    </Section>
+  )
+}
---
+++ src/sections/MoreRecipes/more-recipes.data.js
@@ -0,0 +1,91 @@
+import anotherRecipe1 from '@/assets/images/more-recipes/1.png'
+import anotherRecipe2 from '@/assets/images/more-recipes/2.png'
+import anotherRecipe3 from '@/assets/images/more-recipes/3.png'
+import anotherRecipe4 from '@/assets/images/more-recipes/4.png'
+import anotherRecipe5 from '@/assets/images/more-recipes/5.png'
+import anotherRecipe6 from '@/assets/images/more-recipes/6.png'
+import anotherRecipe7 from '@/assets/images/more-recipes/7.png'
+import anotherRecipe8 from '@/assets/images/more-recipes/8.png'
+
+export const moreRecipes = [
+  {
+    id: 1,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    imgSrc: anotherRecipe1,
+    isLiked: true,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Snack' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Fresh Lime Roasted Salmon with Ginger Sauce',
+    imgSrc: anotherRecipe2,
+    isLiked: false,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Fish' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Strawberry Oatmeal Pancake with Honey Syrup',
+    imgSrc: anotherRecipe3,
+    isLiked: false,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Breakfast' },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Fresh and Healthy Mixed Mayonnaise Salad',
+    imgSrc: anotherRecipe4,
+    isLiked: true,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Healthy' },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Chicken Meatballs with Cream Cheese',
+    imgSrc: anotherRecipe5,
+    isLiked: false,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Meat' },
+    ],
+  },
+  {
+    id: 6,
+    title: 'Fruity Pancake with Orange & Blueberry',
+    imgSrc: anotherRecipe6,
+    isLiked: true,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Sweet' },
+    ],
+  },
+  {
+    id: 7,
+    title: 'The Best Easy One Pot Chicken and Rice',
+    imgSrc: anotherRecipe7,
+    isLiked: false,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Snack' },
+    ],
+  },
+  {
+    id: 8,
+    title: 'The Creamiest Creamy Chicken and Bacon Pasta',
+    imgSrc: anotherRecipe8,
+    isLiked: false,
+    tags: [
+      { icon: 'clock', label: '30 Minutes' },
+      { icon: 'cutlery', label: 'Noodles' },
+    ],
+  },
+]
